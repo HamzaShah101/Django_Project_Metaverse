1033a108733fe5c911dcdda86c217804af9af64f
Hxmza Shah <109261818+HamzaShah101@users.noreply.github.com>
2022-07-19 12:09:15 +0530

    1

--- Martech/MartechMetaverse/urls.py
+++ Martech/MartechMetaverse/urls.py
@@ -4,5 +4,10 @@ from MartechMetaverse import views
 
 urlpatterns = [
     path("",views.index,name="home"),
-    path("news",views.news,name="news")
+    path("news",views.news,name="news"),
+    path("technology",views.technology,name="technology"),
+    path("hr",views.hr,name="hr"),
+    path("Marketing Technology",views.Marketing_Technology,name="marketing_technology"),
+    path("Machine Learning",views.Machine_Learning,name="Machine Learning"),
+    path("Communication",views.communication,name="Communication"),
 ]
--- Martech/MartechMetaverse/views.py
+++ Martech/MartechMetaverse/views.py
@@ -5,4 +5,19 @@ def index(request):
     return render(request, 'index.html')
 
 def news(request):
-    return render(request, 'News.html')
+    return render(request, 'news.html')
+
+def technology(request):
+    return render(request, 'Folder/technology.html')
+
+def hr(request):
+    return render(request, 'Folder/hr.html')
+
+def Marketing_Technology(request):
+    return render(request, 'Folder/marketing_technology.html')
+
+def Machine_Learning(request):
+    return render(request, 'Folder/Machine_Learning.html')
+
+def communication(request):
+    return render(request, 'Folder/communication.html')
