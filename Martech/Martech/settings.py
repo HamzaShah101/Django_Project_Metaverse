be11e52d73999a381c26f93e4bba8fd251c4bcc3
HamzaShah101 <109261818+HamzaShah101@users.noreply.github.com>
2023-05-23 00:48:08 +0530

    Update settings.py

--- Martech/Martech/settings.py
+++ Martech/Martech/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!y3_a1adjfq1$-qg)!15@%=b&upd*q2&ww(@j5a%6ss0*5yjlm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
