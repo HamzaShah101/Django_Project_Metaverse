ce412e0d1a93009da151fb5abdcf3d00d8a244a9
HamzaShah101 <109261818+HamzaShah101@users.noreply.github.com>
2023-05-28 15:49:47 +0530

    Changed- Template

--- Martech/Martech/settings.py
+++ Martech/Martech/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MartechMetaverse'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'Martech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates")],
+        'DIRS': [os.path.join(BASE_DIR,"MartechMetaverse/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,11 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-       ]
+    os.path.join(BASE_DIR, 'static')
+]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
